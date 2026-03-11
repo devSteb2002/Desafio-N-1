5babf59c0dab4be641f445fd29d340989b47c64b
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-11 08:05:54 -0500

    ver posibles colisiones  validaciones.h

--- validaciones.h
+++ validaciones.h
@@ -8,5 +8,8 @@ bool  esMultDeOcho(const unsigned short* numero);
 bool  excedeLimite(const unsigned short* numero);
 bool validacionNumeroIngresado(const unsigned short *numero);
 void validarTipoTablero(void *& tablero, const unsigned short *ancho, const unsigned short *alto,  TipoTablero&  tipotablero);
+bool posicionValida(int fila, int columna, unsigned short alto, unsigned short ancho);
+
+bool hayColision(void* tablero, TipoTablero tipo,int fila,int columna,unsigned short ancho);
 
 #endif // VALIDACIONES_H

a74bca01b0035965c8c64b4db20eb5b67bcc7789
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-10 22:41:13 -0500

    Update main.cpp
    implementación de la creación de piezas en el main

--- main.cpp
+++ main.cpp
@@ -6,26 +6,33 @@ using namespace std;
 
 int main()
 {
-    //variabales globales
-    TipoTablero  tipotablero;
+    //variables globales
+    TipoTablero tipotablero;
 
     unsigned short alto;
     unsigned short ancho;
     void* tablero = nullptr; // sin tipo (para optimizar con el entero)
 
+    Pieza piezas[7];
+
     inicioJuego(&alto, &ancho);
     validarTipoTablero(tablero, &ancho, &alto , tipotablero);
     inicializarTableroEnceros(tablero, tipotablero, &alto);
 
+    cargarPiezas(piezas);
+
+    cout << "\nPieza de prueba:\n";
+    mostrarPieza(piezas[2]);
 
     if(tablero == nullptr) {
         cout << "ERROR: tablero no inicializado!\n";
     }
 
-    //NOTA: verificacion de que si se guarden la cantidad de bits de aguerdo al daot
+    //NOTA: verificacion de que si se guarden la cantidad de bits de acuerdo al dato
 
     for (short i = 0; i < alto; i++) {
         switch(tipotablero) {
+
         case CHAR: {
             cout << "es char " << endl;
             char *filas = static_cast<char*>(tablero);
@@ -34,11 +41,14 @@ int main()
         }
 
         case SHORT:{
-            short * filas = static_cast<short *> (tablero);
+            short * filas = static_cast<short*>(tablero);
             cout << sizeof(filas[i]) * 8 << endl;
             break;
         }
-        case INT: break;
+
+        case INT:
+            break;
+
         case LONG64:
             break;
         }

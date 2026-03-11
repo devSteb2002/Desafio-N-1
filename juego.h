5215fa8c6b8de6589ab22f29d52ebccfd69468a8
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-10 22:13:24 -0500

    Update juego.h
    implementacioon de piezas

--- juego.h
+++ juego.h
@@ -5,4 +5,17 @@
 void  inicioJuego(unsigned short *alto, unsigned short *ancho);
 void inicializarTableroEnceros(void *& tablero, TipoTablero&  tipotablero, const unsigned short *alto);
 
+const int TAM_PIEZA = 4;
+
+
+struct Pieza {
+    char forma[TAM_PIEZA][TAM_PIEZA];
+};
+
+
+void cargarPiezas(Pieza piezas[7]);
+void mostrarPieza(Pieza &p);
+
 #endif // JUEGO_H
+
+

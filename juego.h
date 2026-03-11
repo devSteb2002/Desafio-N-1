864e69cfb3e285aa2037e2dab8f33aa82d41d418
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-11 08:02:15 -0500

    colocar pieza en el tablero juego.h

--- juego.h
+++ juego.h
@@ -5,6 +5,7 @@
 void  inicioJuego(unsigned short *alto, unsigned short *ancho);
 void inicializarTableroEnceros(void *& tablero, TipoTablero&  tipotablero, const unsigned short *alto);
 void dibujarTablero(void *tablero, TipoTablero tipotablero, const unsigned short *alto, const unsigned short *ancho);
+void colocarPieza(void* tablero, TipoTablero tipo, Pieza pieza, int fila,int columna,unsigned short alto, unsigned short ancho);
 
 const int TAM_PIEZA = 4;
 

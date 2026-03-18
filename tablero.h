57bce02fe73eb6cf94a1c035347c15a64f57f05c
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-18 11:07:11 -0500

    Update tablero.h

--- tablero.h
+++ tablero.h
@@ -7,8 +7,5 @@ void inicializarTableroEnceros(void *& tablero, TipoTablero&  tipotablero, const
 void moverCursor(const unsigned short y, const unsigned short x);
 void dibujarTablero(const unsigned short *alto, void *& tablero, TipoTablero&  tipotablero);
 void generarPiezaAleatoria(const unsigned short *alto, void *& tablero);
-const short* obtenerReferenciaPieza(int id);
-void caerPieza(int* fila);
-void proyectarPieza(void* tablero, TipoTablero tipo, const short* pieza, int f, int c);
 
 #endif // TABLERO_H

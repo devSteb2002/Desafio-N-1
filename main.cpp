cfa84ba486db59454fb2da43359cf2b1b99cc21e
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-18 10:44:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,71 +7,49 @@ using namespace std;
 
 int main()
 {
-    //variables globales
     TipoTablero tipotablero;
-
-
     bool juegoActivo = true;
     unsigned short alto;
     unsigned short ancho;
-    void* tablero = nullptr; // sin tipo (para optimizar con el entero)
+    void* tablero = nullptr; 
 
+    
+    const short* pPieza = nullptr; 
+    int filaActual = 0;
+    int colActual = 2; 
 
     inicioJuego(&alto, &ancho);
     validarTipoTablero(tablero, &ancho, &alto , tipotablero);
-    inicializarTableroEnceros(tablero, tipotablero, &alto);
+    
+    
+    pPieza = obtenerReferenciaPieza(0); 
+    
 
     system("cls");
 
     while (juegoActivo){
         moverCursor(0,0);
+
+        
+        inicializarTableroEnceros(tablero, tipotablero, &alto);
+
+ 
+        caerPieza(&filaActual);
+        
+        
+        if (filaActual >= (alto - 4)) filaActual = 0; 
+
+        proyectarPieza(tablero, tipotablero, pPieza, filaActual, colActual);
+
+        
         dibujarTablero(&alto, tablero, tipotablero);
 
-
-
-
-        break;
-
+        Sleep(200); 
     }
-
-
-
-
-    //cout << "\nTABLERO INICIAL\n";
-   // dibujarTablero(tablero, tipotablero, &alto, &ancho);
-
-
 
     if(tablero == nullptr) {
         cout << "ERROR: tablero no inicializado!\n";
     }
-
-    //NOTA: verificacion de que si se guarden la cantidad de bits de acuerdo al dato
-
-   /* for (short i = 0; i < alto; i++) {
-        switch(tipotablero) {
-
-        case CHAR: {
-            cout << "es char " << endl;
-            char *filas = static_cast<char*>(tablero);
-            cout << sizeof(filas[i]) * 8 << endl;
-            break;
-        }
-
-        case SHORT:{
-            short * filas = static_cast<short*>(tablero);
-            cout << sizeof(filas[i]) * 8 << endl;
-            break;
-        }
-
-        case INT:
-            break;
-
-        case LONG64:
-            break;
-        }
-   } */
-
 
     return 0;
 }

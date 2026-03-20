f2e0bea3198a5a01ffda8afb4b5abfcef8ced7a7
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-19 20:11:02 -0500

    Update validaciones.h

--- validaciones.h
+++ validaciones.h
@@ -8,8 +8,8 @@ bool  esMultDeOcho(const unsigned short* numero);
 bool  excedeLimite(const unsigned short* numero);
 bool validacionNumeroIngresado(const unsigned short *numero, bool esAlto = false);
 void validarTipoTablero(void *& tablero, const unsigned short *ancho, const unsigned short *alto,  TipoTablero&  tipotablero);
-bool posicionValida(int fila, int columna, unsigned short alto, unsigned short ancho);
-
-bool hayColision(void* tablero, TipoTablero tipo,int fila,int columna,unsigned short ancho);
+bool esTeclaValida(char tecla);
+bool esPosicionValida(int px, int py, int anchoPieza, int altoPieza, TipoTablero tipo, const unsigned short* altoTablero);
+bool hayColision(void* tablero, TipoTablero tipo, int px, int py, const short* piezaActual, const unsigned short* altoTablero, const unsigned short* anchoTablero);
 
 #endif // VALIDACIONES_H

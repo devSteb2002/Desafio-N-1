b61cd449ed82c04ad50bf035a2327b0555bd0023
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-19 20:07:11 -0500

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -29,6 +29,56 @@ void  inicioJuego(unsigned short *alto, unsigned short *ancho){
     } while (true);
 }
 
+void prepararNuevaPieza(int& px, int& py, int& tipoPieza, int anchoTablero) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 6);
+
+    tipoPieza = dis(gen); 
+    py = 0;               
+    px = (anchoTablero - 4) / 2; 
+}
+void moverPieza(char tecla, int& px, int& py) {
+    if (tecla == 'a' || tecla == 'A') {
+        px++; // Izquierda
+    } 
+    else if (tecla == 'd' || tecla == 'D') {
+        px--; // Derecha
+    } 
+    else if (tecla == 's' || tecla == 'S') {
+        py++; // Bajar
+    }
+}
+void fijarPiezaEnTablero(void* tablero, TipoTablero tipo, int px, int py, int idPieza) {
+    
+    const short* bitsPieza = obtenerDatosPieza(idPieza);
+    
+    if (bitsPieza == nullptr) return;
+
+    for (int i = 0; i < 4; i++) {
+        
+        int filaDestino = py + i;
+        
+        
+        unsigned long long filaDesplazada = (unsigned long long)(*(bitsPieza + i)) << px;
+
+       
+        if (tipo == CHAR_) {
+            *((char*)tablero + filaDestino) |= (char)filaDesplazada;
+        } 
+        else if (tipo == SHORT_) {
+            *((short*)tablero + filaDestino) |= (short)filaDesplazada;
+        } 
+        else if (tipo == INT_) {
+            *((int*)tablero + filaDestino) |= (int)filaDesplazada;
+        } 
+        else if (tipo == LONG64_) {
+            *((long long*)tablero + filaDestino) |= (long long)filaDesplazada;
+        }
+    }
+}
+
+
 
 
 

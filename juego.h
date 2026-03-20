1de6b6351a7af7478b0ff62f5fef6eef8b1a5f14
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-19 20:07:42 -0500

    Update juego.h

--- juego.h
+++ juego.h
@@ -5,6 +5,9 @@
 
 void  inicioJuego(unsigned short *alto, unsigned short *ancho);
 void dibujarTablero(void *tablero, TipoTablero tipotablero, const unsigned short *alto, const unsigned short *ancho);
+void prepararNuevaPieza(int& px, int& py, int& tipoPieza, int anchoTablero);
+void moverPieza(char tecla, int& px, int& py);
+void fijarPiezaEnTablero(void* tablero, TipoTablero tipo, int px, int py, int idPieza);
 
 
 #endif // JUEGO_H

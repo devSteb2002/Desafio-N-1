d8b935030b98132059e3fb6cea83a29fdff774db
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-19 20:26:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include "juego.h"
 #include "tablero.h"
+#include "validaciones.h" 
+#include "piezas.h"
 
 using namespace std;
 
@@ -16,6 +18,10 @@ int main(){
     void* tablero = nullptr; // sin tipo (para optimizar con el entero)
 
 
+    int px, py, idPieza;
+    char tecla;
+
+
 
     inicioJuego(&alto, &ancho);
     validarTipoTablero(tablero, &ancho, &alto , tipotablero);
@@ -23,26 +29,42 @@ int main(){
     generarPiezaAleatoria(&alto, tablero, tipotablero);
 
     system("cls");
+    prepararNuevaPieza(px, py, idPieza, ancho);
+
+
+
 
     while (juegoActivo){
-        moverCursor(0,0);
-        dibujarTablero(&alto, tablero, tipotablero);
+        moverCursor(0, 0);
+        dibujarTablero(&alto, tablero, tipotablero, px, py, idPieza);
 
+        cout << "\n(A/D) Mover, (S) Bajar, (Q) Salir: ";
+        cin >> tecla;
 
-
-
-        break;
-
-    }
-
-
-
-    if(tablero == nullptr) {
-        cout << "ERROR: tablero no inicializado!\n";
-    }
-
-
-
+        if (tecla == 'q' || tecla == 'Q') {
+            juegoActivo = false;
+        } 
+        else if (tecla == 'd' || tecla == 'D') {
+            // A = IZQUIERDA (px - 1)
+            if (!hayColision(tablero, tipotablero, px - 1, py, obtenerDatosPieza(idPieza), &alto, &ancho)) {
+                px--;
+            }
+        }
+        else if (tecla == 'a' || tecla == 'A') {
+            // D = DERECHA (px + 1)
+            if (!hayColision(tablero, tipotablero, px + 1, py, obtenerDatosPieza(idPieza), &alto, &ancho)) {
+                px++;
+            }
+        }
+        else if (tecla == 's' || tecla == 'S') {
+            // S = ABAJO (py + 1)
+            if (!hayColision(tablero, tipotablero, px, py + 1, obtenerDatosPieza(idPieza), &alto, &ancho)) {
+                py++;
+            } else {
+                fijarPiezaEnTablero(tablero, tipotablero, px, py, idPieza);
+                prepararNuevaPieza(px, py, idPieza, ancho);
+            }
+        }
 
 
 

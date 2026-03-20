ef07ef9760ed04541cfad3c8ab45695994153901
miloulou54 <camilo.garcia9@udea.edu.co>
2026-03-19 20:12:35 -0500

    Update validaciones.cpp

--- validaciones.cpp
+++ validaciones.cpp
@@ -87,3 +87,55 @@ void validarTipoTablero(void *& tablero, const unsigned short *ancho, const unsi
     }
 }
 
+
+bool esTeclaValida(char tecla) {
+    if (tecla >= 'A' && tecla <= 'Z') tecla |= 32; 
+    return (tecla == 'a' || tecla == 's' || tecla == 'd' || tecla == 'w' || tecla == 'q');
+}
+
+bool esPosicionValida(int px, int py, int anchoPieza, int altoPieza, TipoTablero tipo, const unsigned short* altoTablero) {
+    int anchoMax = 0;
+    switch(tipo) {
+        case CHAR_: anchoMax = 8; break;
+        case SHORT_: anchoMax = 16; break;
+        case INT_: anchoMax = 32; break;
+        case LONG64_: anchoMax = 64; break;
+    }
+
+    if (px < 0 || (px + anchoPieza) > anchoMax) return false;
+    if (py < 0 || (py + altoPieza) > *altoTablero) return false;
+
+    return true;
+}
+
+
+bool hayColision(void* tablero, TipoTablero tipo, int px, int py, const short* piezaActual, const unsigned short* altoTablero, const unsigned short* anchoTablero) {
+    for (int i = 0; i < 4; i++) {
+        unsigned short filaPieza = piezaActual[i]; 
+        if (filaPieza == 0) continue; 
+
+        int filaDestino = py + i;
+
+        if (px < 0) return true; 
+        if ((unsigned int)px + 4 > *anchoTablero) {
+             if (((unsigned long long)filaPieza << px) >= (1ULL << *anchoTablero)) return true;
+        }
+
+        if (filaDestino >= *altoTablero) return true; 
+
+        if (filaDestino >= 0 && filaDestino < *altoTablero) {
+            unsigned long long filaTablero = 0;
+            switch(tipo) {
+                case CHAR_:   filaTablero = (unsigned char)static_cast<char*>(tablero)[filaDestino]; break;
+                case SHORT_:  filaTablero = (unsigned short)static_cast<short*>(tablero)[filaDestino]; break;
+                case INT_:    filaTablero = (unsigned int)static_cast<int*>(tablero)[filaDestino]; break;
+                case LONG64_: filaTablero = (unsigned long long)static_cast<long long*>(tablero)[filaDestino]; break;
+            }
+
+            if ((filaTablero & ((unsigned long long)filaPieza << px)) != 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
